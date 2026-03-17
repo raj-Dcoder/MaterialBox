9a53ef994ede5a8cbdfd9e9642b850a4bf3a344f
Rajveer <rajveersinghranaofficial@gmail.com>
2026-03-17 16:37:12 +0530

    chore: bump version to 1.2.0 for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ android {
         applicationId = "com.rajveer.materialbox"
         minSdk = 24
         targetSdk = 35
-        versionCode = 2
-        versionName = "1.1.0"
+        versionCode = 3
+        versionName = "1.2.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
